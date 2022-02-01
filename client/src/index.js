2b3abe1c0697fbbbaa1bb12b521b586098274631
Theodosis Katsoulis <theodosis.kats@gmail.com>
2022-02-02 00:34:02 +0200

    persistor import in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import store from './store'
+import { store, persistor } from './store'
 import './index.css';
 import App from './App';
 
